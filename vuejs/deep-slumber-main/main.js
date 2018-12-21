82d97e4ce8de691494896c0081cd18e9a1319de6
Samuel Blattner <samworks@gmx.net>
2018-12-21 08:07:51 +0100

    Use secure websocket

--- vuejs/deep-slumber-main/main.js
+++ vuejs/deep-slumber-main/main.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
 let Vue = window.Vue;
 Vue.use(VueRouter);
 // Vue.use(VueNativeSock, 'ws://localhost:8777', {format: 'json'});
-Vue.use(VueNativeSock, 'ws://deep-slumber.samuelblattner.ch:8777', {format: 'json'});
+Vue.use(VueNativeSock, 'wss://deep-slumber.samuelblattner.ch:8777', {format: 'json'});
 
 const routes = [
     {path: '/', component: DashboardView},
